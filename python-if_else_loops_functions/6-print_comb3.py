5b2fe9a71d8faf4e3d039c0849f95d37ac7abb74
sine shaday <s.sine@alustudent.com>
2023-07-10 13:47:57 -0700

    6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(1, 10):
     for j in range(i+1, 10):
-        print("{:02d}, {:02d}, ".format(i, j), end='')
+         print("{:d}{:d}, ".format(i, j), end='')
 print()
