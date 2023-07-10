d4b4cc959daf59cc01e9f96a406a534a1165a6c2
sine shaday <s.sine@alustudent.com>
2023-07-10 14:25:03 -0700

    8-uppercase.py

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+
 def uppercase(string):
     result = ""
     for char in string:
-        if "a" <= char <= "z":
+        if ord('a') <= ord(char) <= ord('z'):
             result += chr(ord(char) - 32)
         else:
             result += char
     print("{}".format(result))
+
 # Example usage
 uppercase("holberton")
 uppercase("Holberton School")
