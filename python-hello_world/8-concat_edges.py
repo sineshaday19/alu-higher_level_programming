c278f8e6d5d699b1dad875e68612ede7785486d0
sine shaday <s.sine@alustudent.com>
2023-07-11 13:26:23 -0700

    8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive,object-oriented programming language that combines remarkable power with very clear syntax"
-print(str[:68])
-print(str[68:133])
-print(str[133:162])
-print(str[162:174])
-print(str[174:])
+str = "Python is an interpreted, interactive, object-oriented programming\
+         language that combines remarkable power with very clear syntax"
+         str = (str[39:66] + str[106:112] + str[:6])
+         print(str)
