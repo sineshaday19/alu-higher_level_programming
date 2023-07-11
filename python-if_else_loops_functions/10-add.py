cd57c82bcbdfe53218151c3035642c35f239fe32
sine shaday <s.sine@alustudent.com>
2023-07-11 13:49:14 -0700

    10-add.py

--- python-if_else_loops_functions/10-add.py
+++ python-if_else_loops_functions/10-add.py
@@ -1,9 +1,4 @@
-#!/usr/bin/env python3
-
+#!/usr/bin/python3
 def add(a, b):
-    return a + b
-add = __import__('10-add').add
-
-print(add(1, 2))
-print(add(98, 0))
-print(add(100, -2))
+        sum = a + b
+            return sum
