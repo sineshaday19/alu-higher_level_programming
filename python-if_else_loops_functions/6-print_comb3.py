4017d35ab56c8313f4087a4018fa16da4c8a5e85
sine shaday <s.sine@alustudent.com>
2023-07-11 13:38:41 -0700

    6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-for i in range(10):
+
+for i in range(9):
     for j in range(i + 1, 10):
-        print("{:d}{:d}, ".format(i, j), end='')
-print('\n', end='')
+        if (i == 8 and j == 9):
+            print("{num:02}".format(num=((i * 10) + j)), end="\n")
+        else:
+            print("{num:02}".format(num=((i * 10) + j)), end=", ")
