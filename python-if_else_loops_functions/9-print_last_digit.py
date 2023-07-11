6a4d1dafbeda60a956dcc9ba169509b9a3a10c97
sine shaday <s.sine@alustudent.com>
2023-07-11 13:43:07 -0700

    9-print_last_digit.py

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-
-
 def print_last_digit(number):
-    unsigned_num = abs(number)
-    last_digit = unsigned_num % 10
-    print(f"{last_digit}", end="")
-    return (last_digit)
+        last_d = abs(number) % 10
+            print(f"{last_d}", end="")
+                return last_d
