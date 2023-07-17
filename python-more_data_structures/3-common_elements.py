a114dae085044bb983be212fb302138db481e511
Your Name <sineshaday19@github.com>
2023-07-17 13:15:56 -0700

    3-common_elements.py

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -6,4 +6,5 @@ def common_elements(set_1, set_2):
     for element in set_1:
         if element in set_2:
             common_elements_set.add(element)
-            return common_elements_set
+    return common_elements_set
+
