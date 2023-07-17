80a106a8d2d49fa19debd5ac54037fcc804cf85b
Your Name <sineshaday19@github.com>
2023-07-17 13:22:07 -0700

    10-best_score.py

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+
+    best_key = None
+    best_value = float("-inf")
+
+    for key, value in a_dictionary.items():
+        if value > best_value:
+            best_key = key
+            best_value = value
+
+    return best_key
