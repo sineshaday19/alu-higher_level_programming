cfb9f0ac399653d22cbae112126122fb0497e916
Your Name <sineshaday19@github.com>
2023-07-17 12:02:55 -0700

    4-only_diff_elements.py

---
+++ python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+
+def only_diff_elements(set_1, set_2):
+    # Find elements that are present in only one set using symmetric difference (^) operation
+    return set_1 ^ set_2
+
