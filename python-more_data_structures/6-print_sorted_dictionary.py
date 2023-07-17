494e6c4b0356eafbd1c951785faddc4afe51425c
Your Name <sineshaday19@github.com>
2023-07-17 12:46:42 -0700

    6-print_sorted_dictionary.py

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -7,5 +7,10 @@ def print_sorted_dictionary(a_dictionary):
         print(f"{key}: {a_dictionary[key]}")
 
 if __name__ == "__main__":
-    a_dictionary = {'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3]}
+    a_dictionary = {
+        'language': "C",
+        'Number': 89,
+        'track': "Low level",
+        'ids': [1, 2, 3]
+    }
     print_sorted_dictionary(a_dictionary)
