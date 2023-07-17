6209b3277403f2b2a9047b344642a652245cb2d6
Your Name <sineshaday19@github.com>
2023-07-17 12:33:48 -0700

    6-print_sorted_dictionary.py

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def print_sorted_dictionary(a_dictionary):
+    sorted_keys = sorted(a_dictionary.keys())
+    for key in sorted_keys:
+        print(f"{key}: {a_dictionary[key]}")
+
+if __name__ == "__main__":
+    a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
+    print_sorted_dictionary(a_dictionary)
