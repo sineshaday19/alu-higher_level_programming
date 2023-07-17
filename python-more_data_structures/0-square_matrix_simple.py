ed892ea90c6f0e2a5ee7079c0023aa8092a09bd8
Your Name <sineshaday19@github.com>
2023-07-17 12:08:56 -0700

    0-square_matrix_simple.py

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,18 +2,18 @@
 
 
 def square_matrix_simple(matrix=None):
-    # If the matrix parameter is not provided, use an empty list as default
+    #use an empty list as default
     if matrix is None:
         matrix = []
 
-    # Get the number of rows and columns in the matrix
+    #number of rows and columns in the matrix
     n_rows = len(matrix)
     n_cols = len(matrix[0]) if matrix else 0
 
     # Create a new matrix with the same size as the input matrix
     result = [[0 for _ in range(n_cols)] for _ in range(n_rows)]
 
-    # Calculate the square value for each element and store it in the result matrix
+    # Calculate 
     for i in range(n_rows):
         for j in range(n_cols):
             result[i][j] = matrix[i][j] ** 2
