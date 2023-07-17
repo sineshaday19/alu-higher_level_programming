70b3985a5ccf02ccc7e97a8da0ab6b6892e9e42c
Your Name <sineshaday19@github.com>
2023-07-17 13:02:14 -0700

    7-update_dictionary.py

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -3,14 +3,4 @@
 
 def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
-
-# Test the function
-if __name__ == "__main__":
-    my_dict = {'name': 'John', 'age': 30, 'city': 'New York'}
-    print("Original dictionary:", my_dict)
-
-    update_dictionary(my_dict, 'name', 'Alice')
-    print("Updated dictionary:", my_dict)
-
-    update_dictionary(my_dict, 'occupation', 'Engineer')
-    print("Dictionary with added key:", my_dict)
+    return a_dictionary
