76a3c8b88465396ce49c57288141c4e6943fbec4
Your Name <sineshaday19@github.com>
2023-07-17 10:54:45 -0700

    1-search_replace.py

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    new_list = []
+    for item in my_list:
+        if item == search:
+            new_list.append(replace)
+        else:
+            new_list.append(item)
+    return new_list
+
+# Test the function with the provided example
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
+    new_list = search_replace(my_list, 2, 89)
+    print(new_list)  # Output: [1, 89, 3, 4, 5, 4, 89, 1, 1, 4, 89]
+    print(my_list)   # Output: [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
+
