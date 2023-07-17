b4971729d01d65cfe1b78899e2c97d98b9333ac4
Your Name <sineshaday19@github.com>
2023-07-17 15:41:22 -0700

    6-raise_exception_msg.py

--- python-exceptions/6-raise_exception_msg.py
+++ python-exceptions/6-raise_exception_msg.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 
-def raise_exception_msg(message=""):
+def raise_exception_msg(message="write anything"):
     if not message:
         raise NameError("Empty message is not allowed.")
     else:
