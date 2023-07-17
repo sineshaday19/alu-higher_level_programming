f615f0858ed53afe2ec3f6c5247a23371f0c1c03
Your Name <sineshaday19@github.com>
2023-07-17 13:08:58 -0700

    3-common_elements.py

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -2,13 +2,10 @@
 
 
 def common_elements(set_1, set_2):
-    common = set()
+    common_elements_set = set()
+    
     for element in set_1:
         if element in set_2:
-            common.add(element)
-    return common
-
-set_1 = {"Python", "C", "Javascript"}
-set_2 = {"Bash", "C", "Ruby", "Perl"}
-c_set = common_elements(set_1, set_2)
-print(sorted(list(c_set)))i
+            common_elements_set.add(element)
+            
+    return common_elements_set
