7b767b397ccdf5a3c4fb4fb0a1e8e7a903f2fce1
Your Name <sineshaday19@github.com>
2023-07-17 15:55:46 -0700

    4-list_division.py

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 
-def list_divisionmylist1 mylist1 = [10, 0, 4] / mylist2 = [2, 4, 0] / list_length = 3
+def list_divisionmylist1 (my_list_1, my_list_2, list_length):
     result = []
     
     for i in range(list_length):
