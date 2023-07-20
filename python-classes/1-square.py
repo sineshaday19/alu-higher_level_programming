483055cb24ca2a1e9cc2866f2c7da5f0f90305f7
Your Name <sineshaday19@github.com>
2023-07-20 09:04:17 -0700

    1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module: 1-square
+Private instance attribute: size
+"""
+
+
+class Square:
+    """
+    Class Square: Represents a square
+    """
+    def __init__(self, size):
+        """
+        Initializes a Square instance with a given size.
+        """
+        self.__size = size
