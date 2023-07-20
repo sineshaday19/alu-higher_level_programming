99343e5c376985bf3f05edee2a0d661922d0f009
Your Name <sineshaday19@github.com>
2023-07-20 08:30:33 -0700

    2-safe_print_list_integers.py

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-
-
 def safe_print_list_integers(my_list=[], x=0):
-    count = 0
-    for item in my_list:
+    """Print the first x elements of a list that are integers.
+    Args:
+        my_list (list): The list to print elements from.
+        x (int): The number of elements of my_list to print.
+    Returns:
+        The number of elements printed.
+    """
+    ret = 0
+    for i in range(0, x):
         try:
-            # Try to convert the item to an integer
-            num = int(item)
-            # Print the integer using "{:d}".format() to format it
-            print("{:d}".format(num), end=' ')
-            count += 1
-        except ValueError:
-            # Skip non-integer values in the list
-            pass
-
-    print()  # Print a new line after all integers are printed
-    if count < x:
-        raise ValueError("Number of elements to access (x) is greater than the length of my_list.")
-
-    return count
+            print("{:d}".format(my_list[i]), end="")
+            ret += 1
+        except (ValueError, TypeError):
+            continue
+    print("")
+    return (ret)
