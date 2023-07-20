98e8351c3af094257bb9953e66717c1786e4c0d6
Your Name <sineshaday19@github.com>
2023-07-20 09:13:39 -0700

    3-square.py

---
+++ python-classes/3-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Module: 3-square
+Private instance attribute: size
+"""
+
+
+class Square:
+    """
+    Class Square: Represents a square
+    """
+    def __init__(self, size=0):
+        """
+        Initializes a Square instance with a given size (default is 0).
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """
+        Calculates and returns the current square area.
+        """
+        return self.__size * self.__size
