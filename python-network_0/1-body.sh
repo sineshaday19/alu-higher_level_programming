2b36c7187598744e4948183add501a7ab3c50bee
Your Name <sineshaday19@github.com>
2023-08-04 13:53:09 -0700

    1-body.sh

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a DELETE request to the URL passed as argument and displays the response
-curl -sX DELETE "$1"
+# sends a GET request to the URL, and displays the body of the response
+curl -sfL "$1"
