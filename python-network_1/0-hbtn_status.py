0e94fe467977067bb1977a3dfb953e2dde2be043
Your Name <sineshaday19@github.com>
2023-08-14 07:45:17 -0700

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
+"""Python script that fetches https://alu-intranet.hbtn.io/status"""
+
 
 import urllib.request
 
-if __name__ == "__main__":
-    url = 'https://alu-intranet.hbtn.io/status'
 
-    with urllib.request.urlopen(url) as response:
-        body = response.read()
-        utf8_content = body.decode('utf-8')
-
-    print("Body response:")
-    print(f"    - type: {type(body)}")
-    print(f"    - content: {body}")
-    print(f"    - utf8 content: {utf8_content}")
+if __name__ == '__main__':
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode("utf-8")))
