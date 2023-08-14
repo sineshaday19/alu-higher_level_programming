1e7b0b51a037418657a092ac7f1c8f72372e1dc2
Your Name <sineshaday19@github.com>
2023-08-14 07:51:37 -0700

    2-post_email.py

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''a Python script that takes in a URL and an email,
+sends a POST request to the passed URL with the email
+as a parameter, and displays the body of the
+response (decoded in utf-8)
+'''
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    params = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(params)
+    data = data.encode('ascii')
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode('utf-8'))
